b39d86348f12f1056920c17d711aa7db6fa9489f
Diego Alvares de Macedo <101260437+diegoalvares@users.noreply.github.com>
2023-09-22 17:29:00 -0300

    aplicando efeito de animação.

--- script.js
+++ script.js
@@ -10,20 +10,33 @@ btn.addEventListener('click', function(e){
         const dolar = dinheiro.value*4.93
         console.log(dolar)
         resultado.innerHTML = '🤑 $ ' + dolar + ' 🤑'
+
+        animacao();
     }
     else if (moeda.value == 'Euro'){
         const euro = dinheiro.value*5.25
         console.log(euro)
         resultado.innerHTML = '🤑 € ' + euro + ' 🤑'
+
+        animacao();
     }
 
     else if (moeda.value == 'Ien'){
         const ien = dinheiro.value*0.033
         console.log(ien)
         resultado.innerHTML= '🤑 ¥ ' + ien + ' 🤑'
+
+        animacao();
     }
 
     else{
         alert('Preencha todos os campos!')
     }
 })
+
+function animacao(){
+    return resultado.animate([
+        {transform: 'translateY(-60px)'},
+        {transform: 'translateY(0px)'},
+    ], {duration: 500})
+};
