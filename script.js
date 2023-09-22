b22987c6827beb467710293595a19ae5b3f8f91a
Diego Alvares de Macedo <101260437+diegoalvares@users.noreply.github.com>
2023-09-22 14:59:38 -0300

    atualizando

---
+++ script.js
@@ -0,0 +1,25 @@
+const btn = document.querySelector('#converter')
+
+btn.addEventListener('click', function(e){
+    e.preventDefault();
+    const dinheiro = document.getElementById('dinheiro')
+    const moeda = document.getElementById('moedas')
+    const resultado = document.getElementById('resultado')
+
+    if (moeda.value == 'Dólar'){
+        const dolar = dinheiro.value*4.93
+        console.log(dolar)
+        resultado.innerHTML = '🤑 R$ ' + dolar + ' 🤑'
+    }
+    else if (moeda.value == 'Euro'){
+        const euro = dinheiro.value*5.25
+        console.log(euro)
+        resultado.innerHTML = '🤑 R$ ' + euro + ' 🤑'
+    }
+
+    else if (moeda.value == 'Ien'){
+        const ien = dinheiro.value*0.033
+        console.log(ien)
+        resultado.innerHTML= '🤑 R$ ' + ien + ' 🤑'
+    }
+})
